f936e3017e92a9c31712c228e6675e47bfa7baff
Roberto Alsina <roberto.alsina@gmail.com>
2020-03-03 15:00:59 -0300

    more stuff

--- roke/main.py
+++ roke/main.py
@@ -1,2 +1,12 @@
+import click
+
+smallnum = [str(i) for i in range(15)]
+basic = "the quick brown fox jumped over the lazy dog".split()
+
+@click.command()
+@click.option("--dicts", default="basic,smallnum", help="What list of words should be used.")
+@click.option("--format", default="{smallnum}-{basic}-{basic}", help="Format for the generated identifier.")
+@click.option("--count", default=1, help="How many identifiers to generate.")
+
 def main():
     pass
