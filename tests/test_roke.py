723166f2573e4119215f8ed621cb932f402ea444
Roberto Alsina <roberto.alsina@gmail.com>
2020-03-06 13:38:34 -0300

    Added basic test for get_dict_list

--- tests/test_roke.py
+++ tests/test_roke.py
@@ -1,6 +1,8 @@
+from pathlib import Path
+
 import hypothesis.strategies as st
 from hypothesis import given
-from roke.main import gen_identifiers
+from roke.main import gen_identifiers, get_dict_list
 
 
 @given(st.integers(min_value=0, max_value=10000))
@@ -8,3 +10,13 @@ def test_count(count):
     pattern = ""
     result = gen_identifiers(pattern, count)
     assert len(result) == count
+
+
+def test_get_dict_list(monkeypatch):
+    monkeypatch.setattr(Path, "is_dir", lambda *a: True)
+    monkeypatch.setattr(Path, "glob", lambda p, _: [p / "1.txt", p / "2.txt"])
+    result = get_dict_list()
+    # Results are 2 things per place, there are 3 places
+    assert len(result) == 6
+    # Results are all distinct
+    assert len(set(result)) == 6
